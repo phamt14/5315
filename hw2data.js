ebbff3f5607507258aea6d02957480027edd0c84
god <phamt14@seattleu.edu>
2018-04-29 21:59:40 -0700

    hw2 draft

--- hw2data.js
+++ hw2data.js
@@ -1,12 +1,52 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "email": "newemail@gmail.com"
+      "Name": "James Bond",
+      "Marital Status": "Single and Always Available",
+      "Weakness": "Alcoholism",
+      "email": "ohohseven@gmail.com"
     },
     {
-      "name": "Brielle Williamson",
-      "email": "jbond@su.edu"
+      "Name": "Liam Payne",
+      "Marital Status": "Maried to a lady much older than himself",
+      "Weakness": "Can only move in One Direction",
+      "email": "cradle_robbed@uk.com"
+    },
+    {
+      "Name": "Adolf Hitler",
+      "Marital Status": "Married to Eva Braun",
+      "Weakness": "Nein",
+      "email": "DasReich@deustch.com"
+    },
+    {
+      "Name": "Voldemort",
+      "Marital Status": "Married to a snake",
+      "Weakness": "A boy wizard name Harry",
+      "email": "tom.r.marvolo@hw.edu"
+    },
+    {
+      "Name": "Kim Jong Un",
+      "Marital Status": "Maried but adored by all females in North Korea",
+      "Weakness": "Sweets. Dude is obese.",
+      "email": "rocketman@northk.com"
+    },
+    {
+      "Name": "Taylor Swift",
+      "Marital Status": "No idea",
+      "Weakness": "Nice face boys",
+      "email": "richgirl@gmail.com"
+    },
+    {
+      "Name": "Margaret Thatcher",
+      "Marital Status": "Maried",
+      "Weakness": "Bloody Treacherous Micheal Heseltin",
+      "email": "argentina.conqueror.co.uk"
+    },
+    {
+      "Name": "Nicki Minaj",
+      "Marital Status": "Engaged to a boy toy name Troy",
+      "Weakness": "Men",
+      "email": "myanacondadont@su.edu"
     }
   ]
 }
